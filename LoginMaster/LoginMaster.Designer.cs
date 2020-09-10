7dbc5fb36d973b67305b2113d62603f101a7b93b
Alex Moffet <asmoffet@ksu.edu>
2020-09-10 10:42:39 -0500

    have the other

---
+++ LoginMaster/LoginMaster.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace LoginMaster
+{
+    partial class LoginMaster
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbStateMessage = new System.Windows.Forms.Label();
+            this.uxLoginBtn = new System.Windows.Forms.Button();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.tbUserName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbStateMessage
+            // 
+            this.lbStateMessage.AutoSize = true;
+            this.lbStateMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbStateMessage.Location = new System.Drawing.Point(55, 15);
+            this.lbStateMessage.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbStateMessage.Name = "lbStateMessage";
+            this.lbStateMessage.Size = new System.Drawing.Size(0, 20);
+            this.lbStateMessage.TabIndex = 11;
+            // 
+            // uxLoginBtn
+            // 
+            this.uxLoginBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxLoginBtn.Location = new System.Drawing.Point(88, 194);
+            this.uxLoginBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.uxLoginBtn.Name = "uxLoginBtn";
+            this.uxLoginBtn.Size = new System.Drawing.Size(92, 41);
+            this.uxLoginBtn.TabIndex = 10;
+            this.uxLoginBtn.Text = "Login";
+            this.uxLoginBtn.UseVisualStyleBackColor = true;
+            this.uxLoginBtn.Click += new System.EventHandler(this.UxLoginBtn_Click);
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPassword.Location = new System.Drawing.Point(136, 125);
+            this.tbPassword.Margin = new System.Windows.Forms.Padding(2);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '#';
+            this.tbPassword.Size = new System.Drawing.Size(126, 26);
+            this.tbPassword.TabIndex = 9;
+            // 
+            // tbUserName
+            // 
+            this.tbUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbUserName.Location = new System.Drawing.Point(136, 68);
+            this.tbUserName.Margin = new System.Windows.Forms.Padding(2);
+            this.tbUserName.Name = "tbUserName";
+            this.tbUserName.Size = new System.Drawing.Size(126, 26);
+            this.tbUserName.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 122);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Password:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 65);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "User Name:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 267);
+            this.Controls.Add(this.lbStateMessage);
+            this.Controls.Add(this.uxLoginBtn);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbUserName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbStateMessage;
+        private System.Windows.Forms.Button uxLoginBtn;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.TextBox tbUserName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ LoginMaster/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginMaster
+{
+    public delegate void InputHandler(State state, String args);
+    public delegate void StateObs(State state);
+    static class Program
+    {
+        
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            
+            CredentialsMP model = new CredentialsMP();
+            Controller controller = new Controller(model);
+            LoginMaster view = new LoginMaster(controller.handleEvents);
+            controller.regesterObs(view.DisplayState);
+            Application.Run(view);
+        }
+    }
+}
